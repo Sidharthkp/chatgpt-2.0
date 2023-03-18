00661b55b32e78967217a6cdd4744550373147db
Sidharthkp <sidharthkannan20@gmail.com>
2023-03-18 14:13:49 +0530

    profile added in sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,9 @@
+'use client';
+import { useSession } from "next-auth/react"
 import NewChat from "./NewChat"
 
 function SideBar() {
+    const { data: session } = useSession();
     return (
         <div className="p-2 flex flex-col h-screen">
             <div className="flex-1">
@@ -13,6 +16,7 @@ function SideBar() {
                     {/* Map through the chat rows */}
                 </div>
             </div>
+            {session && <img src={session.user?.image!} alt="Profile picture" className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50" />}
         </div>
     )
 }
